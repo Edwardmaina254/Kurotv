a47d51b89f7a03cedbf6e418e6778631b1abc8be
Edward <felixnjuguna31@gmail.com>
2026-08-18 19:06:05 +0300

    chore: remove test scripts and debug files

--- test_gogo_ep.cjs
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-const https = require('https');
-const agent = new https.Agent({ rejectUnauthorized: false });
-
-axios.get('https://anitaku.bz/giant-killing-episode-1', { httpsAgent: agent }).then(r => {
-  const match = r.data.match(/<iframe[^>]+src=["']([^"']+)["']/i);
-  console.log(match ? match[1] : 'No iframe found');
-}).catch(console.error);
